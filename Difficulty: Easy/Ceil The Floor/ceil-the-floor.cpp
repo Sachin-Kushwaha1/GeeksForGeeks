65cb9c5b30b1ab7809cbb76079e0c8704317f308
Sachin Kumar <110785295+Sachin-Kushwaha1@users.noreply.github.com>
2024-09-10 18:12:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User code template
+
+class Solution {
+  public:
+    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+        // code here
+        int fl = 0, cl = INT_MAX;
+        for(auto it:arr){
+            if(it<=x && it>fl) fl = it;
+            if( it>=x && it<cl ) cl = it;
+        }
+        if(fl == 0) fl=-1;
+        if(cl == INT_MAX) cl=-1;
+        return {fl,cl};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+    while (t--) {
+        vector<int> arr;
+        int x;
+        string input;
+        cin >> x;
+        cin.ignore();
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.getFloorAndCeil(x, arr);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
